50c9ca595e22f12c64e70ea72130fe084b45c781
chanshunli <chanshunli@gmail.com>
2017-12-02 00:05:52 +0800

    .col-sm-*柱子

--- src/girls_coding_cljs/core.cljs
+++ src/girls_coding_cljs/core.cljs
@@ -1,5 +1,5 @@
 (ns girls-coding-cljs.core
-    (:require [reagent.core :as reagent :refer [atom]]))
+  (:require [reagent.core :as reagent :refer [atom]]))
 (enable-console-print!)
 
 (defonce app-state (atom {:text "Hello world!"}))
@@ -9,12 +9,20 @@
    [:h1 (:text @app-state)]
    [:h3 "Edit this and watch it change!"]])
 
+(defn column-sm
+  []
+  "两个柱子"
+  [:div.row>div.col-sm-2>div.row
+   [:div.col-sm-6 "第一个柱子"]
+   [:div.col-sm-6 "第二个柱子"]])
+
 (reagent/render-component
- [hello-world]
+ [#_(fn []
+      ) hello-world]
  (. js/document (getElementById "app")))
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
+  )
