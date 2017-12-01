69b4309d02cedae2b5ff7154b94aced34daea881
chanshunli <chanshunli@gmail.com>
2017-12-02 00:27:41 +0800

    基础面板和列表导航

--- src/girls_coding_cljs/core.cljs
+++ src/girls_coding_cljs/core.cljs
@@ -1,14 +1,16 @@
 (ns girls-coding-cljs.core
   (:require [reagent.core :as reagent :refer [atom]]))
+
+;; TODOS remove
 (enable-console-print!)
-
 (defonce app-state (atom {:text "Hello world!"}))
-
+(defn on-js-reload [])
 (defn hello-world []
   [:div
    [:h1 (:text @app-state)]
    [:h3 "Edit this and watch it change!"]])
 
+;; The cljs books
 (defn column-sm
   []
   "两个柱子"
@@ -16,13 +18,21 @@
    [:div.col-sm-6 "第一个柱子"]
    [:div.col-sm-6 "第二个柱子"]])
 
+(defn panel
+  []
+  [:div.panel.panel-default>div.panel-body "基础面板---带边框文本显示块"])
+
+(defn list-group
+  []
+  [:div.list-group>a.list-group-item
+   [:h4.list-group-item-heading "自定义列表组"]
+   [:p.list-group-item-text "这里是..."]])
+
+(defn list-group-ul-li
+  []
+  [:ul.list-group>li.list-group-item "导航栏" [:span.badge "徽章1"]])
+
 (reagent/render-component
  [#_(fn []
       ) hello-world]
  (. js/document (getElementById "app")))
-
-(defn on-js-reload []
-  ;; optionally touch your app-state to force rerendering depending on
-  ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
-  )
